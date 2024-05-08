d9d026e87dd22dec350504fa94621c81c67021df
Lingrode <grimaloilya@gmail.com>
2024-05-07 18:38:05 -0600

    update all components

---
+++ src/components/Categories.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = [
+    "Усі",
+    "М'ясні",
+    "Вегетаріанська",
+    "Гриль",
+    "Гострі",
+    "Закриті",
+  ];
+
+  const onClickIndex = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, i) => (
+          <li
+            onClick={() => onClickIndex(i)}
+            className={activeIndex === i ? "active" : ""}
+          >
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
