7814548b705e37bae1b6e2a5dc8a11fc5006b1af
Lingrode <grimaloilya@gmail.com>
2024-05-13 19:00:26 -0600

    installed React Redux and Redux Toolkit, created test couner with Redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+// import { Routes, Route } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import Cart from "./pages/Cart";
+// import Header from "./components/Header";
+// import Home from "./pages/Home";
+// import NotFound from "./pages/NotFound";
+// import Cart from "./pages/Cart";
+import { decrement, increment } from "./redux/slices/filterSlice";
 
 import "./scss/app.scss";
 
@@ -13,20 +15,39 @@ export const SearchContext = React.createContext();
 console.log(SearchContext);
 
 function App() {
-  const [searchValue, setSearchValue] = useState("");
+  // const [searchValue, setSearchValue] = React.useState("");
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
   return (
     <div className="wrapper">
-      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+      <div>
+        <div>
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
+      {/* <SearchContext.Provider value={{ searchValue, setSearchValue }}>
         <Header />
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="*" element={<NotFound />} />
-            {/* <Home /> */}
           </Routes>
         </div>
-      </SearchContext.Provider>
+      </SearchContext.Provider> */}
     </div>
   );
 }
