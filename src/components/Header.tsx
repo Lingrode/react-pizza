a253982608cf022db23bfb6f2324bd005adb5f3b
Lingrode <grimaloilya@gmail.com>
2024-05-25 19:26:35 -0600

    realized saving pizzas in LocalStorage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,13 +8,22 @@ import { selectCart } from "../redux/slices/cartSlice";
 
 function Header() {
   const { items, totalPrice } = useSelector(selectCart);
-
+  const isMounted = React.useRef(false);
   const location = useLocation();
 
   const totalCount = items.reduce(
     (sum: number, item: any) => sum + item.count,
     0
   );
+
+  React.useEffect(() => {
+    if (isMounted.current) {
+      const json = JSON.stringify(items);
+      localStorage.setItem("cart", json);
+    }
+
+    isMounted.current = true;
+  }, [items]);
 
   return (
     <div className="header">
