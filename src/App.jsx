30e00ebc1894c2297e6d8c35957e1bc1b5815881
Illia Hrymalo <grimaloilya@gmail.com>
2025-01-01 17:40:46 -0700

    fetch pizzas from mockAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 import "./scss/app.scss";
 
-import pizzas from "./assets/pizzas.json";
-
 const App = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://663c26aa17145c4d8c354a8e.mockapi.io/items")
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -19,7 +25,7 @@ const App = () => {
           </div>
           <h2 className="content__title">All pizzas</h2>
           <div className="content__items">
-            {pizzas.map((pizza) => {
+            {items.map((pizza) => {
               return <PizzaBlock key={pizza.id} {...pizza} />;
             })}
           </div>
