56dc8480c37b69550c07188ea53af0c286446e9d
Illia Hrymalo <grimaloilya@gmail.com>
2025-01-16 22:53:24 -0700

    update

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filterSlice from "./slices/filterSlice";
-import cartSlice from "./slices/cartSlice";
+import cartSlice from "./slices/cartSlice.ts";
 import pizzaSlice from "./slices/pizzaSlice";
 
 export const store = configureStore({
